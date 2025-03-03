8e968b9721afdb8b79fad716853d6a99082c523a
juan <juancarlosgmzany@gmail.com>
2025-03-02 23:57:44 -0400

    cambios 2

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,11 +3,15 @@ import { pool } from '../db.js'
 
 export const getCI = async (req, res) => {
     const { ci } = req.params
-    const {rows} = await pool.query('SELECT * FROM userCI ', [ci]);
-    if(rows.length == 0) {
-        return res.status(404).json({message: "Usuario no encontrado"}) 
+    try {
+        const { rows } = await pool.query('SELECT * FROM userCI ', [ci]);
+        if (rows.length == 0) {
+            return res.status(404).json({ message: "Usuario no encontrado" })
+        }
+        res.json(rows)
+    } catch (error) {
+        res.json({...error})
     }
-    res.json(rows)
 }
 
 export const getActive = (req, res) => {
