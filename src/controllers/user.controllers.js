3297ec5dc42fa08d0ef56c9849862e03b3a995be
juan <juancarlosgmzany@gmail.com>
2025-03-02 20:47:17 -0400

    cambios

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,9 +3,13 @@ import { pool } from '../db.js'
 
 export const getCI = async (req, res) => {
     const { ci } = req.params
-    const {rows} = await pool.query('SELECT * FROM UserCI WHERE ci = $1', [ci]);
+    const {rows} = await pool.query('SELECT * FROM userCI ', [ci]);
     if(rows.length == 0) {
         return res.status(404).json({message: "Usuario no encontrado"}) 
     }
     res.json(rows)
+}
+
+export const getActive = (req, res) => {
+    res.json({ message: "ESTOY ACTIVO" })
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { getCI } from '../controllers/user.controllers.js';
+import { getActive, getCI } from '../controllers/user.controllers.js';
 
 const router = Router();
 
 router.get("/user/:ci", getCI)
+router.get("/", getActive)
 
 export default router
